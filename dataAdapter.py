2142d4234e07dc60603a06b37a306355c2e989de
DuyPham <duy.cs.pham@gmail.com>
2023-10-01 14:34:11 -0500

    model and data adapter for User and Restaurant

--- dataAdapter.py
+++ dataAdapter.py
@@ -1,5 +1,5 @@
 import sqlite3
-from models import Address
+from models import Address, User, Restaurant
 
 
 class dataAdapter():
@@ -7,16 +7,46 @@ class dataAdapter():
         self.db = db
         self.conn = sqlite3.connect(db)
         self.cursor = self.conn.cursor()
+    
+    def close(self):
+        self.conn.close()
 
     
     def saveAddress(self, address):
-        self.cursor.execute("INSERT INTO addresses VALUES (?,?,?,?,?)", (address.address_id, address.street, address.city, address.state, address.zip))
+        self.cursor.execute("INSERT INTO addresses (street, city, state, zip) VALUES (?,?,?,?)", (address.street, address.city, address.state, address.zip))
         self.conn.commit()
     
     def getAddress(self, address_id):
         self.cursor.execute("SELECT * FROM addresses WHERE address_id=?", (address_id,))
         row = self.cursor.fetchone()
         return Address(row[0], row[1], row[2], row[3], row[4])
+    
+    def deleteAddress(self, address_id):
+        self.cursor.execute("DELETE FROM addresses WHERE address_id=?", (address_id,))
+        self.conn.commit()
+    
+    def saveUser(self, user):
+        self.cursor.execute("INSERT INTO users (username, password, first_name, last_name, email, user_type) VALUES (?,?,?,?,?,?)", (user.username, user.password, user.first_name, user.last_name, user.email, user.user_type))
+        self.conn.commit()
+
+    def getUser(self, user_id):
+        self.cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
+        row = self.cursor.fetchone()
+        return User(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+    
+    def deleteUser(self, user_id):
+        self.cursor.execute("DELETE FROM users WHERE user_id=?", (user_id,))
+        self.conn.commit()
+
+    def saveRestaurant(self, restaurant):
+        self.cursor.execute("INSERT INTO restaurants (name, address_id, phone_number, email, description) VALUES (?,?,?,?,?)", (restaurant.name, restaurant.address_id, restaurant.phone_number, restaurant.email, restaurant.description))
+        self.conn.commit()
+
+    def getRestaurant(self, restaurant_id):
+        self.cursor.execute("SELECT * FROM restaurants WHERE restaurant_id=?", (restaurant_id,))
+        row = self.cursor.fetchone()
+        return Restaurant(row[0], row[1], row[2], row[3], row[4], row[5])
+    
     
 
 
@@ -25,5 +55,15 @@ class dataAdapter():
 
 dataAdapter = dataAdapter("food.db")
 
+# dataAdapter.saveAddress(Address(None, "123 Main St", "New York", "NY", "10001"))
+# dataAdapter.saveUser(User(None, "johndoe", "password", "John", "Doe", "johndoe@gmail.com", "customer"))
+# dataAdapter.saveRestaurant(Restaurant(None, "McDonalds", 1, "123-456-7890", "", "Fast Food"))
 
-dataAdapter.saveAddress(Address(1, "123 Main St", "New York", "NY", "10001"))
+# address = dataAdapter.getAddress(1)
+# user = dataAdapter.getUser(1)
+restaurant = dataAdapter.getRestaurant(1)
+
+print(restaurant)
+
+
+dataAdapter.close()
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 class Address():
-    def __init__(self, address_id, street, city, state, zip):
+    def __init__(self, address_id ,street, city, state, zip):
         self.address_id = address_id
         self.street = street
         self.city = city
@@ -7,12 +7,15 @@ class Address():
         self.zip = zip
 
     def __str__(self):
-        return f"{self.street}, {self.city}, {self.state}, {self.zip}"
+        return f"{self.address_id}, {self.street}, {self.city}, {self.state}, {self.zip}"
 
     def __repr__(self):
-        return f"{self.street}, {self.city}, {self.state}, {self.zip}"
+        return f"{self.address_id}, {self.street}, {self.city}, {self.state}, {self.zip}"
     
     # setters
+    def set_address_id(self, address_id):
+        self.address_id = address_id
+
     def set_street(self, street):
         self.street = street
 
@@ -26,6 +29,9 @@ class Address():
         self.zip = zip
     
     # getters
+    def get_address_id(self):
+        return self.address_id
+    
     def get_street(self):
         return self.street
     
@@ -37,4 +43,122 @@ class Address():
     
     def get_zip(self):
         return self.zip
+
+class User():
+    def __init__(self, user_id ,username, password, first_name, last_name, email, user_type):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.user_type = user_type
+
+    def __str__(self):
+        return f"{self.user_id}, {self.username}, {self.password}, {self.first_name}, {self.last_name}, {self.email}, {self.user_type}"
+
+    def __repr__(self):
+        return f"{self.user_id}, {self.username}, {self.password}, {self.first_name}, {self.last_name}, {self.email}, {self.user_type}"
+    
+    # setters
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_password(self, password):
+        self.password = password
+
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+    
+    def set_last_name(self, last_name):
+        self.last_name = last_name
+    
+    def set_email(self, email):
+        self.email = email
+
+    def set_user_type(self, user_type):
+        self.user_type = user_type
+    
+    
+    
+    # getters
+    def get_user_id(self):
+        return self.user_id
+    
+    def get_username(self):
+        return self.username
+    
+    def get_password(self):
+        return self.password
+    
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.last_name
+    
+    def get_email(self):
+        return self.email
+    
+    def get_user_type(self):
+        return self.user_type
+    
+
+class Restaurant():
+    def __init__(self, restaurant_id, name, address_id, phone_number, email, description):
+        self.restaurant_id = restaurant_id
+        self.name = name
+        self.address_id = address_id
+        self.phone_number = phone_number
+        self.email = email
+        self.description = description
+
+
+    def __str__(self):
+        return f"{self.restaurant_id}, {self.name}, {self.address_id}, {self.phone_number}, {self.email}, {self.description}"
+
+    def __repr__(self):
+        return f"{self.restaurant_id}, {self.name}, {self.address_id}, {self.phone_number}, {self.email}, {self.description}"
+    
+    # setters
+    def set_restaurant_id(self, restaurant_id):
+        self.restaurant_id = restaurant_id
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_address_id(self, address_id):
+        self.address_id = address_id
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+    
+    def set_email(self, email):
+        self.email = email
+    
+    def set_description(self, description):
+        self.description = description
+
+    
+    # getters
+    def get_restaurant_id(self):
+        return self.restaurant_id
+    
+    def get_name(self):
+        return self.name
+    
+    def get_address_id(self):
+        return self.address_id
+    
+    def get_phone_number(self):
+        return self.phone_number
+    
+    def get_email(self):
+        return self.email
+    
+    def get_description(self):
+        return self.description
     
